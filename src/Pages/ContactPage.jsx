0f0de01b56a7bb3c6fbdb5246e06d5d1da880e69
guptaRishi00 <rishigupta54066@gmail.com>
2025-06-24 23:09:35 +0530

    .

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -216,7 +216,7 @@ const ContactPage = () => {
                       onChange={handleInputChange}
                       disabled={isSubmitting}
                       className="w-full px-4 py-3 bg-gray-700/50 border border-gray-600 rounded-lg focus:ring-2 focus:ring-orange-500 focus:border-transparent transition-all duration-200 text-white placeholder-gray-400 disabled:opacity-50"
-                      placeholder="+1 (555) 123-4567"
+                      placeholder="+91 9646443444"
                     />
                   </div>
                   <div>
